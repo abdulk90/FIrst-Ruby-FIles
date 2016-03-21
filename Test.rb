6e260cc6294581745b56b24e5d5005847d8e1fbd
Abdul <abdul.kayyali@gmail.com>
2016-03-21 09:19:25 -0400

    Initial commit, Word Sorting

---
+++ Test.rb
@@ -0,0 +1,13 @@
+puts "first word?"
+a = gets.chomp
+puts "second word?"
+b = gets.chomp
+puts "thid word?"
+c = gets.chomp
+puts "forth word"
+d = gets.chomp
+
+puts "The words in order:"
+
+puts [a,b,c,d].upcase
+
