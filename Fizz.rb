d3e97a124a3d83699ec8267106ec37f906be6e14
abdulk90 <abdul.kayyali@gmail.com>
2016-03-21 10:14:02 -0400

    Added "Bang!

--- Fizz.rb
+++ Fizz.rb
@@ -2,7 +2,11 @@
 x = 0
 while x<30
 	x = x +1
-	if x % 3 == 0 && x % 5 ==0
+	if x == 1
+		puts "Bang!"
+	elsif x > 9 && x <20
+		puts "BuzzBang!"
+	elsif x % 3 == 0 && x % 5 ==0
 		puts "FizzBizz!"
 	elsif x % 3 == 0
 		puts "Fizz"
